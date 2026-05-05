cd0dc5f450679668ad3a9d692f52dadb86efe1f2
samhermeli <samuel.heredia@mercadolibre.cl>
2026-05-05 16:52:53 -0400

    fix: export MercadoPagoSftp credential in index, bump to 0.2.6 (#61)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,5 @@
 import { MercadoPagoApi } from './credentials/MercadoPagoApi.credentials';
 import { MercadoPago } from './nodes/MercadoPago/MercadoPago.node';
-export declare const version = "0.1.0";
 export declare const credentials: {
     name: string;
     class: typeof MercadoPagoApi;
